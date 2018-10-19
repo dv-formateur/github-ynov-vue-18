b63da2bb506e30b8f971d5edba8c97eaaa50f732
mrolland <maxime.rolland23@outlook.com>
2018-10-19 10:08:20 +0200

    Initialisation du projet

---
+++ src/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <div class="uk-position-top">
+      <h1>Application github ynov</h1>
+      <form>
+        <label class="uk-form-label">Choix du projet</label>
+          <select class="uk-select">
+              <option></option>
+              <option></option>
+          </select>
+          <label class="uk-form-label">Choix de la periode</label>
+             <date-picker i18n="EN"> </date-picker>
+          <label class="uk-form-label">Choix du compte</label>
+          <select class="uk-select">
+              <option></option>
+              <option></option>
+          </select>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import UIkit from 'uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
+import VueRangedatePicker from 'vue-rangedate-picker'
 
 Vue.config.productionTip = false
+
+// Ajout du datepicker range 
+Vue.use(VueRangedatePicker) // using it
+Vue.component('DatePicker', VueRangedatePicker)
+// Utilisation de UIkit
+UIkit.use(Icons)
 
 new Vue({
   render: h => h(App)
